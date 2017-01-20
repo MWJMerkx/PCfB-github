ca2a8abeb6b30d5ff5fb758509afdc6ad280df48
Michiel Merkx <m.w.j.merkx@student.rug.nl>
2017-01-20 13:16:04 +0000

    Added images to the file

--- TheGradeIDeserveIs.mk
+++ TheGradeIDeserveIs.mk
@@ -1,4 +1,4 @@
-I would like an 8 pretty please.
+I would like a 7-8 pretty please.
 
 
 
@@ -7,7 +7,7 @@ made an issue on https://github.com/Lumphie/Fairytale_test
 accepted invitation to collaborate on https://github.com/Lumphie/Fairytale_test
 closed issue
 
-!(/screenshots/Issues.png)
+![issues](/screenshots/Issues.png)
 
 cloned repository
 
@@ -16,31 +16,41 @@ cloned repository
 eddited part 1
 pushed part 1 back with comment "version something"
 
+![push](/screenshots/successfullpush.png)
 
 
 updated profile picture
 
-
+![profile](/screenshots/profilepicture.png)
 
 fixed merge conflict
+
+![mergeconflict](screenshots/mergeconflict.png)
 
 
 
 Create fairytale_test fork
 
-
+![fork](/screenshots/fork.png)
 
 edited part two and created pull request
 
-
+![pullrequest](screenshots/pullrequest.png)
 
 accepted pull request
 
 
 made a branch "crispy"
 
+![branch](screenshots/newbranch.png)
+![merge](screenshots/mergebranch.png)
+
 fixed merge errors "crispy"
+
+![branchmerge](/screenshots/fixedbranchmerge.png)
 
 
 pull request and merge "crispy" with "master"
 
+![pull](/screenshots/pullrequest2.png)
+![pull](/screenshots/pullrequest3.png)
